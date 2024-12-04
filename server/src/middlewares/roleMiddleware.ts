22ceac9b4afd3c7c06b7ebc5c09991f9a02ba314
Aman Verma <aman@Amans-MacBook-Air.local>
2024-12-04 20:15:16 +0530

    complete authorize role middleware

---
+++ server/src/middlewares/roleMiddleware.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+
+const authorizeRoles = (...allowedRoles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    //@ts-ignore
+    const { role } = req.user;
+    if (!allowedRoles.includes(role)) {
+      return res.status(403).json({ message: "Forbidden" });
+    }
+    next();
+  };
+};
+
+export default authorizeRoles;
+// In the code snippet above, we created a middleware function that checks if the role of the user making the request is included in the list of allowed roles. If the user's role is not included in the list of allowed roles, the middleware function returns a 403 Forbidden status code. Otherwise, the function calls the next middleware function in the stack.
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,21 +1,40 @@
 import express from "express";
 import { Request, Response } from "express";
 import verifyToken from "../middlewares/authMiddleware";
+import authorizeRoles from "../middlewares/roleMiddleware";
 const router = express.Router();
 
 // only admin can access this route
-router.get("/admin", verifyToken, (req: Request, res: Response) => {
-  res.status(200).json({ message: "Admin route" });
-});
+router.get(
+  "/admin",
+  verifyToken,
+  //@ts-ignore
+  authorizeRoles("admin"),
+  (req: Request, res: Response) => {
+    res.status(200).json({ message: "Admin route" });
+  }
+);
 
 // Both admin and manager can access this route
-router.get("/manager", verifyToken, (req: Request, res: Response) => {
-  res.status(200).json({ message: "Manager route" });
-});
+router.get(
+  "/manager",
+  verifyToken,
+  //@ts-ignore
+  authorizeRoles("admin", "manager"),
+  (req: Request, res: Response) => {
+    res.status(200).json({ message: "Manager route" });
+  }
+);
 
 // All can access this route
-router.get("/user", verifyToken, (req: Request, res: Response) => {
-  res.status(200).json({ message: "User route" });
-});
+router.get(
+  "/user",
+  verifyToken,
+  //@ts-ignore
+  authorizeRoles("admin", "manager", "user"),
+  (req: Request, res: Response) => {
+    res.status(200).json({ message: "User route" });
+  }
+);
 
 export default router;
